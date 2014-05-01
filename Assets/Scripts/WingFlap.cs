e513d07e7ab3793397aed956f0b52f7f12fa292e
Niclas Bach Nielsen <niclas.bach@gmail.com>
2014-05-01 12:51:25 +0200

    WingFlap.cs added

---
+++ Assets/Scripts/WingFlap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WingFlap : MonoBehaviour {
+
+	public GameObject human;
+
+	ZigSkeleton skeletonScript;
+	Vector3 wingZ;
+	float rotationAmount = 0.1f;
+	Vector3 torsoZ;
+	Vector3 tempZ;
+
+	// Use this for initialization
+	void Start () {
+		skeletonScript = human.GetComponent<ZigSkeleton>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Rotates the body left and right depending sideways movement of the user
+		wingZ = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, skeletonScript.RightShoulder.transform.localEulerAngles.z);
+		//tempZ = skeletonScript.RightShoulder.transform.localEulerAngles.z;
+		transform.localEulerAngles = -tempZ;
+		tempZ = wingZ;
+	}
+}
